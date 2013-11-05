b92e382683d70a8ae21ea8bbddc9c51dd12cdb28
jgoldd <jgoldd@fullsail.edu>
2013-11-04 21:30:04 -0700

    Slice of pie part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -6,3 +6,15 @@ const humanYears = 7;
 var age = dogAge * humanYears;
 //print out of "dog age" based on human years
 console.log(age)
+
+
+//How much pizza each party goer will recieve
+const slices = 8;
+var people = 25;
+var pizzas = 10;
+//pizzas ordered times the number of slices, divided by the number of people & asign slicePerson variable
+
+var slicePerson = (slices * pizzas)/people;
+//print hout how much each person ate
+console.log(slicePerson)
+
