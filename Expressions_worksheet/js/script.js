6768f133d6700d087cb1b6d23388d9c0e76fb23c
jgoldd <jgoldd@fullsail.edu>
2013-11-04 21:04:07 -0700

    Added Dog Years Expression
    testing knowledge.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+//Age of Sparky in dog years based on actual age
+var dogAge = 2.5;
+const humanYears = 7;
+
+// multiply dogAge by humanYears & assign to age variable
+var age = dogAge * humanYears;
+//print out of "dog age" based on human years
+console.log(age)
