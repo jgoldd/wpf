edabd3f9df678cb246d90f474107f86b76fe5957
jgoldd <jgoldd@fullsail.edu>
2013-11-05 13:58:04 -0700

    added average, cant seem to figure out part 2 of slice of pie

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,20 +1,39 @@
 //Age of Sparky in dog years based on actual age
-var dogAge = 2.5;
-const humanYears = 7;
+var humanYears = 2.5; //dogs age in human years 
+const dogAge = 7;
 
 // multiply dogAge by humanYears & assign to age variable
-var age = dogAge * humanYears;
+var age = humanYears * dogAge;
 //print out of "dog age" based on human years
-console.log(age)
+console.log("Spark is" + " " + humanYears + " " + "which is" + " " + age + " " + "in dog years.")
 
 
 //How much pizza each party goer will recieve
-const slices = 8;
-var people = 25;
-var pizzas = 10;
+const slices = 3; //Number of slices 8
+var people = 8; //Number of people 25
+var pizzas = 10; //Number of pizzas 10
+
 //pizzas ordered times the number of slices, divided by the number of people & asign slicePerson variable
-
 var slicePerson = (slices * pizzas)/people;
-//print hout how much each person ate
-console.log(slicePerson)
+//print hout how many pieces per person
+console.log("Each person ate" + " " + slicePerson + " " + "slices of pizza at the party.")
 
+
+
+//Sparky gets what remainder of pizza
+//Slices by pizzas used modulo to give remainder with people
+var dogFood = 30 % people;
+//print out how many slices sparky got
+console.log("Sparky got" + " " + dogFood + " " + "slices of pizza.");
+
+
+//total amount spent on groceries and average
+var groceryTotals = [75.87, 45.56, 89.34, 67.49, 91.01];
+
+//Add of all weeks of groceries 
+var total = groceryTotals[0] + groceryTotals[1] + groceryTotals[2] + groceryTotals[3] + groceryTotals[4];
+//dividing all weeks of groceries by number of weeks for average
+var average = total/5;
+
+//print out of average spent of groceries and total
+console.log("You have spent a total of" + " " + total + " " + "on groceries over 5 weeks. That is an average of" + " " + average + " " + "per week.") 
