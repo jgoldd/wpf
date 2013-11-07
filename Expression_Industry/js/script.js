3111b2fdc7de01da21dc17191dc590b1cc0b6b5a
jgoldd <jgoldd@fullsail.edu>
2013-11-07 13:46:08 -0700

    added area hight prompt

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -9,6 +9,7 @@
 
 //tiling an image for background with size of tile and size of area
 
-var tileWidth = prompt("What is the width (inches) of the image:")
-var tileHeight = prompt("What is the Height (inches) of the image:")
-var areaWidth = prompt("What is the width (feet) of the area:")
+var tileWidth = prompt("What is the width (inches) of the image:");
+var tileHeight = prompt("What is the Height (inches) of the image:");
+var areaWidth = prompt("What is the width (feet) of the area:");
+var areaHeight = prompt("What is the height (feet) of the area:");
