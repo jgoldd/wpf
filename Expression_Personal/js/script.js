dee80510ef14ef7a1925107b69e5b6ef868b1c40
jgoldd <jgoldd@fullsail.edu>
2013-11-07 16:50:22 -0700

    added array into console

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -12,8 +12,8 @@ const monthlyRent = 1175; //Monthly rent constant
 var electricBill = prompt("Please enter Electric Bill amount:"); //Electric bill variable in prompt 
 var utilityBill = prompt("Please enter Utility Bill amount:"); //Utility bill variable in prompt
 var cableBill = prompt("Please enter Cable Bill amount:"); //Cable bill variable in prompt
-var roomMates = 4; //Number of occupants in home in prompt
-var roomMates = ["Justin", "Josh", "Alex", "Ryan"]
+var roomMateTotal = 4; //Number of occupants in home in prompt
+var roomMates = ["Justin", "Josh", "Alex", "Ryan"] //added array for bills split too
 
 //Adding up electric bill/utilitybill/cablebill
 var monthlyTotal = monthlyRent + Number(electricBill) + Number(utilityBill) + Number(cableBill);
@@ -21,4 +21,4 @@ var monthlyTotal = monthlyRent + Number(electricBill) + Number(utilityBill) + Nu
 var monthlySplit = monthlyTotal/roomMates;
 
 //print out monthly total and total split among occupants
-console.log("Your total for rent is " + "$" + monthlyTotal + " and split between " + roomMates + " occupants it is " + "$" + monthlySplit + " a piece.");
+console.log("Your total for rent is " + "$" + monthlyTotal + " and split between " + roomMateTotal + " occupants it is " + "$" + monthlySplit + " a piece." roomMates[0] + " recieves " + utilityBillSeprate + "from " + roomMates[1] + ", " + roomMates[2] + ", " roomMates[3] for Comcast and Electricity." );
