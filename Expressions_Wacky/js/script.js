b4d2f462446be661fec1ee113232e4804f81386c
jgoldd <jgoldd@fullsail.edu>
2013-11-07 16:21:47 -0700

    added formula for aspect ratio

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -11,10 +11,16 @@
 //Aspect ratio of original image to new image sizes
 var originalHeight = prompt("Enter start image height:"); // starting image demensions height
 var originalWidth = prompt("Enter start image width:"); //starting image demensions width
-var newWidth = prompt("Enter target deminsion width:"); //new deminsion width
+var newHeight = prompt("Enter target deminsion height:"); //new deminsion width
 
 
-// divide original height by original width to get aspect ratio number 1
-var newHeight = (originalHeight / originalWidth) * newWidth;
+//divide original height by original width to get aspect ratio number 1
+var newWidth = (originalHeight / originalWidth) * newWidth;
 
-console.log("Your new height is ")
+
+//getting aspect ratio
+var aspectRatioOne = originalHeight / originalWidth;
+var aspectRatioTwo = newHeight / newWidth;
+
+
+console.log("Your new height is " + newHeight)
