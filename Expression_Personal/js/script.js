6c7e335913a41efc65c769c1aa3c9a9574e14e95
jgoldd <jgoldd@fullsail.edu>
2013-11-07 11:00:47 -0700

    Finding monthly rent comment added

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -1,1 +1,11 @@
-alert("hello world!")
+/*
+	Assignment: Expressions Personal Assignment
+	
+	Author: Justin Goldman
+	
+	Date: November 7, 2013
+	
+*/
+
+
+//Finding out monthly rent divided by number of people
