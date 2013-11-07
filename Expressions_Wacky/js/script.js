703004b49577000e18dfd1a4a57741f1e3c1979d
jgoldd <jgoldd@fullsail.edu>
2013-11-07 16:33:44 -0700

    added px unit

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,14 +9,13 @@
 
 
 //Aspect ratio of original image to new image si
-var originalHeight = prompt("Enter start image height:"); // starting image demensions height
-var originalWidth = prompt("Enter start image width:"); //starting image demensions width
-var newWidth = prompt("Enter target deminsion width:"); //new deminsion width
+var originalHeight = prompt("Enter start image height (px):"); // starting image demensions height
+var originalWidth = prompt("Enter start image width: (px)"); //starting image demensions width
+var newWidth = prompt("Enter target deminsion width: (px)"); //new deminsion width
 
 
 //divide original height by original width to get aspect ratio number 1
 var newHeight = (originalHeight / originalWidth) * newWidth;
 
 
-
-console.log("Your new height is " + newHeight);
+console.log("Your new height is " + newHeight + " pixels.");
