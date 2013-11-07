ce68c9b03104db9d616e9a8f38f2a388af042375
jgoldd <jgoldd@fullsail.edu>
2013-11-07 16:40:51 -0700

    added comment for concatenating

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -13,7 +13,7 @@
 var originalHeight = prompt("Enter start image height (px):"); // starting image demensions height
 var originalWidth = prompt("Enter start image width: (px)"); //starting image demensions width
 var newWidth = prompt("Enter target deminsion width: (px)"); //new deminsion width
-var newImage = "Your new image is ";
+var newImage = "Your new image is "; // Concatenating for print out
 var pixels = " pixels.";
 
 
