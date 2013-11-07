6191632ed9d1ba52a5633540b06664ea1a5a0688
jgoldd <jgoldd@fullsail.edu>
2013-11-07 13:01:21 -0700

    Added assignment title and comment for title

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -1,1 +1,10 @@
-alert("hello world!")
+/*
+	Assignment: Expressions Industry Assignment
+	
+	Author: Justin Goldman
+	
+	Date: November 7, 2013
+	
+*/
+
+//Converting PX to EM and Percentages based on Font Size 16px
