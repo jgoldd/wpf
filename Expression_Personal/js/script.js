1a79a2195d1435ba3b337fa163af7fc10374e191
jgoldd <jgoldd@fullsail.edu>
2013-11-07 11:40:23 -0700

    Added part of calculation for monthly split

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -13,5 +13,10 @@ const monthlyRent = 1175; //Monthly rent constant
 var electricBill = prompt("Please enter Electric Bill amount:"); //Electric bill variable in prompt 
 var utilityBill = prompt("Please enter Utility Bill amount:"); //utility bill variable in prompt
 var cableBill = prompt("Please enter Cable Bill amount:"); //Cable bill variable in prompt
+var roomMates = prompt("Please enter amount of occupants:"); //Number of occupants in home in prompt
 
-//Calculate 
+//Calculate monthly rent for 4 people
+
+var monthlySplit = monthlyRent + Number(electricBill);
+
+console.log(monthlySplit);
