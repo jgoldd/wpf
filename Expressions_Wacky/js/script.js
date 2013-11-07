ce580cc910cc223147328831b9cdd3667c7afa87
jgoldd <jgoldd@fullsail.edu>
2013-11-07 16:38:34 -0700

    adding first line of string concatenation

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,9 +9,11 @@
 
 
 //Aspect ratio of original image to new image size using pixels and new mage width
+
 var originalHeight = prompt("Enter start image height (px):"); // starting image demensions height
 var originalWidth = prompt("Enter start image width: (px)"); //starting image demensions width
 var newWidth = prompt("Enter target deminsion width: (px)"); //new deminsion width
+var newImage = "Your new image is ";
 
 
 //divide original height by original width to get new height
