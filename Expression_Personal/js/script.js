e9c37e9530112b155268334d60c097956a459948
jgoldd <jgoldd@fullsail.edu>
2013-11-07 11:04:40 -0700

    Added utility bill variable

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -11,3 +11,5 @@
 //Finding out monthly rent divided by number of people
 const monthlyRent = 1175; //Monthly rent constant
 var electricBill = 110; //Electric bill variable 
+var utilityBill = 110; //utility bill variable
+
