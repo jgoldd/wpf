e5cbe9fc3916d4c62a1c29e6e5ae773b022164e3
jgoldd <jgoldd@fullsail.edu>
2013-11-07 13:47:12 -0700

    calculate area of tile

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -13,3 +13,6 @@ var tileWidth = prompt("What is the width (inches) of the image:");
 var tileHeight = prompt("What is the Height (inches) of the image:");
 var areaWidth = prompt("What is the width (feet) of the area:");
 var areaHeight = prompt("What is the height (feet) of the area:");
+
+//Calculate area of the tile 
+var tileArea = tileWidth * tileHeight;
