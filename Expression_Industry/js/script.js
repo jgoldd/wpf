55fe62fbff6e39186ad3eb7314b1b116e7929585
jgoldd <jgoldd@fullsail.edu>
2013-11-07 14:03:49 -0700

    added comments

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -12,8 +12,8 @@
 var tileWidth = prompt("What is the width (inches) of the image:"); //Image Width in Prompt
 var tileHeight = prompt("What is the Height (inches) of the image:"); //Image Height in Prompt
 var areaWidth = prompt("What is the width (feet) of the area:"); //Width of area in Prompt
-var areaHeight = prompt("What is the height (feet) of the area:");
-const areaSquareFeet = 144;
+var areaHeight = prompt("What is the height (feet) of the area:"); //Height of area in Prompt
+const areaSquareFeet = 144; //Square feet in 12 x 12 constant
 
 //Calculate area of the tile 
 var tileArea = tileWidth * tileHeight;
