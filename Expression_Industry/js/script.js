8a7ebd8abdad0e5c650ac168cb41ae055ff3d5b6
jgoldd <jgoldd@fullsail.edu>
2013-11-07 14:03:02 -0700

    added comments

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -9,9 +9,9 @@
 
 //tiling an image for background with size of tile and size of area
 
-var tileWidth = prompt("What is the width (inches) of the image:");
-var tileHeight = prompt("What is the Height (inches) of the image:");
-var areaWidth = prompt("What is the width (feet) of the area:");
+var tileWidth = prompt("What is the width (inches) of the image:"); //Image Width in Prompt
+var tileHeight = prompt("What is the Height (inches) of the image:"); //Image Height in Prompt
+var areaWidth = prompt("What is the width (feet) of the area:"); //Width of area in Prompt
 var areaHeight = prompt("What is the height (feet) of the area:");
 const areaSquareFeet = 144;
 
@@ -23,5 +23,5 @@ var areaTotal = areaWidth * areaHeight;
 
 var total = areaTotal / tileAreaTotal;
 
-console.log("The total number of " + tileWidth + " x " + tileHeight + "that could fit in a " + areaWidth + " x " + areaHeight + "is " + total);
+console.log("The total number of " + tileWidth + " x " + tileHeight + " images that could fit in a " + areaWidth + " x " + areaHeight + " area is " + total);
 
