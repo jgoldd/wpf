f3d96d53241b6a21b2af0b8742397d3c98fa4d9f
jgoldd <jgoldd@fullsail.edu>
2013-11-12 16:24:05 -0700

    Added comments for each line

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -11,13 +11,17 @@ var carMpg = 32;
 var gasPercentage = .25;
 var tankCapacity = 30;
 var milesToGo = 200;
-//
+//Car MPG times the Tanks Capacity for overall Gas Median
 var gasMedian = carMpg * tankCapacity;
+//Gas overall times Percenatage of gas in tank
 var gasLeft = gasMedian * gasPercentage;
+//If amount of gas left is more than 200 
 
 if(gasLeft > milesToGo){
+	//if more than 200
     console.log("Yes, you can make it without stopping for gas");
 	
 }else {
+	//if it's less or equal to 200
     console.log("You only have " + gasLeft + " gallons of gas in your tank, better get gas now while you can");
 }
