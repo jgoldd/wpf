ed7f2737ab8cf02699ef85e887178c24ae263641
jgoldd <jgoldd@fullsail.edu>
2013-11-12 16:13:01 -0700

    Added code for first expression testing

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -6,3 +6,18 @@
 	Date: November 5, 2013
 	
 */
+//Collect MPG, Gas Tank Percentage left, and Gas Tank Capacity 
+var carMpg = prompt("Miles per gallon?", "MPG");
+var gasPercentage = prompt("Gas percentage left in tank?", "%");
+var tankCapacity = prompt("Car's gas tank capacity?(in Gallons)");
+var milesToGo = 200;
+//
+var gasMedian = carMpg * tankCapacity;
+var gasLeft = gasMedian * gasPercentage;
+
+if(gasLeft > milesToGo){
+    console.log("Yes, you can make it without stopping for gas");
+	
+}else {
+    console.log("You only have " + gasLeft + " gallons of gas in your tank, better get gas now while you can");
+}
