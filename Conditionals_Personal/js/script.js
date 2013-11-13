004508a28bb71db4edddf11db9553c21c8866810
jgoldd <jgoldd@fullsail.edu>
2013-11-13 13:05:22 -0700

    Added time now variable

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,4 +7,5 @@
 	
 */
 //Have I slept long enough?
-var sleepTime = prompt("What time did yo go to bed?") //Time layed down for bed
+var sleepTime = prompt("What time did yo go to bed?"); //Time layed down for bed
+var timeNow = prompt("What time is it now?"); //Time it is now
