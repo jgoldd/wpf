5c05bff0853b391bc2f8871061c0301893542ff7
jgoldd <jgoldd@fullsail.edu>
2013-11-13 21:25:38 -0700

    added how many views array

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,8 +9,8 @@
 //Advertisment pay for monthly clicks on a site 
 var totalClickPerAd = prompt("What is the total number of monthly clicks per AD?"); //clicks per AD 
 var totalAds = prompt("What is the total number of Ads?"); //How many ads you have
-var adPay = [.20, .50];
-var adPayView = [200, 400];
+var adPay = [.20, .50]; //ad pay per views
+var adPayView = [200, 400]; //views total for pay
 
 var totalClicks = totalClickPerAd * totalAds; //total clicks per ad times number of ads
 var lowPagePay = totalClicks * adPay[0]; //overall clicks times pay of .20
