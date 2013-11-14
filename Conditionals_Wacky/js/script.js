2144fa3b6a71b39de38c6af2633ab11b43acd8cd
jgoldd <jgoldd@fullsail.edu>
2013-11-14 10:57:37 -0700

    added MPG var and comment

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,3 +9,4 @@
 
 //can you make it by 5:00 to the meeting walking leaving at 3:00
 var distance = prompt("What is the distance you will be walking in miles?"); //Amount of distance to walk
+var mpg = prompt("Average walking speed in MPG?"); //Average speed of walk in MPG
