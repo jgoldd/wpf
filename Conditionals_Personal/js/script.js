aed67b53acb17dd90cae92e8ecbab5e84736b0fd
jgoldd <jgoldd@fullsail.edu>
2013-11-13 17:37:16 -0700

    changed conditional expression

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,8 +6,7 @@
 	Date: November 13, 2013
 	
 */
-//Have I slept long enough?
-var sleepTime = prompt("What time did yo go to bed?"); //Time layed down for bed
-var timeNow = prompt("What time is it now?"); //Time it is now
-var validation = "";
+//Can I survive this month on my SSI disability
+var totalIncome = prompt("What is your monthly income"); //Check from SSI check
+var rentTotal = prompt("What is your rent total?"); //Rent total amount
 
