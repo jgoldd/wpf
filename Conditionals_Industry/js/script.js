aba26a59ec2357a4a825fa4368a5c1797e1f31f4
jgoldd <jgoldd@fullsail.edu>
2013-11-13 21:10:05 -0700

    print out added for 200 print

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -17,6 +17,6 @@ var lowPagePay = totalClicks * adPay[0]; //overall clicks times pay of .20
 var highPagePay = totalClicks * adPay[1]; //overall clicks times pay of .50
 
 if (totalClicks  >= adPayView[0]) { //if total clicks is greater or equal to 200 print to console
-
+	console.log("You had a total of " + totalClicks + " clicks from " + totalAds + " Ads. You made " + lowPagePay + " from Ad clicks. ");
 }
  
