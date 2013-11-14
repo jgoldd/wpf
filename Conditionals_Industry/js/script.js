4c1d65fa612c2eb6d5cf47ec964d73dda9de59ac
jgoldd <jgoldd@fullsail.edu>
2013-11-13 21:04:08 -0700

    Addedd adpay by amount

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,5 +7,6 @@
 	
 */
 //Advertisment pay for monthly clicks on a site 
-var totalViews = prompt("What is the total number of monthly clicks?");
-var totalAds = prompt("What is the total number of Ads?")
+var totalClickPerAd = prompt("What is the total number of monthly clicks per AD?");
+var totalAds = prompt("What is the total number of Ads?");
+var adPay = [.20, .50];
