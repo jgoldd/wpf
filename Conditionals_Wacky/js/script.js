91b53c358963fe438e414518864420efc759a64a
jgoldd <jgoldd@fullsail.edu>
2013-11-14 10:58:27 -0700

    added const for time remaining

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -10,3 +10,4 @@
 //can you make it by 5:00 to the meeting walking leaving at 3:00
 var distance = prompt("What is the distance you will be walking in miles?"); //Amount of distance to walk
 var mpg = prompt("Average walking speed in MPG?"); //Average speed of walk in MPG
+const timeLeft = 2; //time left to make it to meeting. can be changed based on time remaining
