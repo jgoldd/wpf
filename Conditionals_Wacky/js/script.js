4a85e65a2148387da237ac206ab1b415de47899f
jgoldd <jgoldd@fullsail.edu>
2013-11-14 11:20:15 -0700

    added comments for print out

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -14,5 +14,8 @@ const timeLeft = 2; //time left to make it to meeting. can be changed based on t
 var validation = ""; //validation of user input in prompt
 
 var time = distance / mpg; //distance to walk by walking mpg for total time to walk
+//if no input in distance or mpg, print to console you did not enter 
+//if time is less than time left then print true
+//if total hours pass time to walk print false
 
 (validation === distance && validation === mpg) ? console.log("You did not enter a distance or an average walking speed.") : (time < timeLeft) ? console.log("Yes! You will make it! You have " + timeLeft + "hours to make it to the meeting and it will only take you " + time + " hour(s).") : console.log("No, you will not make it on time.");
