a99c0bfeaaa1bc97855f6cffeaf5879dfcf21cee
jgoldd <jgoldd@fullsail.edu>
2013-11-14 10:27:12 -0700

    added validation var

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,10 +11,12 @@ var totalClickPerAd = prompt("What is the total number of monthly clicks per AD?
 var totalAds = prompt("What is the total number of Ads?"); //How many ads you have
 var adPay = [.20, .50]; //ad pay per views
 var adPayView = [200, 400]; //views total for pay
+var validation = "";
 
 var totalClicks = totalClickPerAd * totalAds; //total clicks per ad times number of ads
 var lowPagePay = totalClicks * adPay[0]; //overall clicks times pay of .20
 var highPagePay = totalClicks * adPay[1]; //overall clicks times pay of .50
+
 
 if(totalClicks >= adPayView[0] && totalClicks < adPayView[1]) { //if total clicks is greater or equal to 200 print to console && total clicks is less than 400
 	console.log("You had a total of " + totalClicks + " clicks from " + totalAds + " Ads. You made " + "$" + lowPagePay + " from Ad clicks. ");
