9242a2607847b03d5e4d01b9e7abe64e0b657f85
jgoldd <jgoldd@fullsail.edu>
2013-11-13 21:27:17 -0700

    Added $ symbol to console print out

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -17,9 +17,9 @@ var lowPagePay = totalClicks * adPay[0]; //overall clicks times pay of .20
 var highPagePay = totalClicks * adPay[1]; //overall clicks times pay of .50
 
 if(totalClicks >= adPayView[0] && totalClicks < adPayView[1]) { //if total clicks is greater or equal to 200 print to console && total clicks is less than 400
-	console.log("You had a total of " + totalClicks + " clicks from " + totalAds + " Ads. You made " + lowPagePay + " from Ad clicks. ");
+	console.log("You had a total of " + totalClicks + " clicks from " + totalAds + " Ads. You made " + "$" + lowPagePay + " from Ad clicks. ");
 }else if(totalClicks >= adPayView[1]) { //if total clicks is greater or equal to 400 print to console
-	console.log("You had a total of " + totalClicks + " clicks from " + totalAds + " Ads. You made " + highPagePay + " from Ad clicks. ");
+	console.log("You had a total of " + totalClicks + " clicks from " + totalAds + " Ads. You made " + "$" + highPagePay + " from Ad clicks.");
 }else { //if under 200 clicks you get nothing, print out
 	console.log("You did not make 200 clicks or over this month, you only had " + totalClicks + " clicks. Which there is now pay out for under 200 clicks.");
 }
