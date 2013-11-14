94b0e89624504d35207828b295627b3286792146
jgoldd <jgoldd@fullsail.edu>
2013-11-13 20:17:21 -0700

    testing expression of total views and total ads

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,3 +6,6 @@
 	Date: November 13, 2013
 	
 */
+//Advertisment pay for monthly clicks on a site 
+var totalViews = prompt("What is the total number of monthly clicks?");
+var totalAds = prompt("What is the total number of Ads?")
