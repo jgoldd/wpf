7a3a57eb301e7c7f0ca019a170ba89e3db5149c4
jgoldd <jgoldd@fullsail.edu>
2013-11-14 11:04:02 -0700

    validation var added

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -11,6 +11,7 @@
 var distance = prompt("What is the distance you will be walking in miles?"); //Amount of distance to walk
 var mpg = prompt("Average walking speed in MPG?"); //Average speed of walk in MPG
 const timeLeft = 2; //time left to make it to meeting. can be changed based on time remaining
+var validation = ""; //validation of user input in prompt
 
 var time = distance / mpg; //distance to walk by walking mpg for total time to walk
 
