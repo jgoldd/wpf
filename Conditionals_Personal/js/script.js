32bb5ed75ab703fd68fa24ba2f809bc13bb8a4da
jgoldd <jgoldd@fullsail.edu>
2013-11-13 20:08:45 -0700

    added comments to each line

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -15,15 +15,15 @@ var validation = "";
 var expensesTotal = Number(rentTotal) + Number(otherTotal); //Expenses added together
 var leftOvers = totalIncome - expensesTotal; //Total left over
 
-if(validation === totalIncome){
-	console.log("You did not enter a monthly income.");
-}else if(validation === rentTotal){
-	console.log("You did not enter a rent total.");
-}else if(validation === otherTotal){
-	console.log("You did not enter a expenses total.");
-}else if(totalIncome > expensesTotal){
+if(validation === totalIncome){ //check for user input in monthy income prompt
+	console.log("You did not enter a monthly income."); //Print if no input in monthly prompt
+}else if(validation === rentTotal){ //check for user input in rent total prompt
+	console.log("You did not enter a rent total."); //Print if no input in total prompt
+}else if(validation === otherTotal){ //check for user input in other expenses prompt
+	console.log("You did not enter a expenses total."); //Print if no input in expenses prompt
+}else if(totalIncome > expensesTotal){ //if total income is greater than expenses print to console
 	console.log("You can survive this month! You will spend " + "$" + expensesTotal + " in rent and other expenses. You will have " + "$" + leftOvers + " left for the month.");
-}else {
+}else { //if total expenses goes over monthly income print this to console
 	console.log("You do not have enough money. Your income is " + "$" + totalIncome + " and you need " + "$" + expensesTotal + ".");
 }
 	
