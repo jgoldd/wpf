81f36313440e58306419685d25e42473a4a9d642
jgoldd <jgoldd@fullsail.edu>
2013-11-13 18:02:10 -0700

    added validation

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -12,10 +12,13 @@ var rentTotal = prompt("What is your rent total?"); //Rent total amount
 var otherTotal = prompt("Any other necessary expenses?"); //Any other expenses
 var validation = ""; //validation of user input
 
-var expensesTotal = rentTotal + otherTotal; //Expenses added together
+var expensesTotal = Number(rentTotal) + Number(otherTotal); //Expenses added together
 var leftOvers = totalIncome - expensesTotal; //Total left over
 
-if(expensesTotal > totalIncome){
+if(expensesTotal < totalIncome){
 	console.log("You can survive this month! You will spend " + "$" + expensesTotal + " in rent and other expenses. You will have " + "$" + leftOvers + " left for the month.");
+	
+}else if(validation === totalIncome){
+	console.log("You did not enter a value.");
 	
 }
