637a27c386dc1275fb5ede9bc1e24d6a684c2ef6
jgoldd <jgoldd@fullsail.edu>
2013-11-13 21:24:01 -0700

    add comments for total clicks and how many ads

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,8 +7,8 @@
 	
 */
 //Advertisment pay for monthly clicks on a site 
-var totalClickPerAd = prompt("What is the total number of monthly clicks per AD?");
-var totalAds = prompt("What is the total number of Ads?");
+var totalClickPerAd = prompt("What is the total number of monthly clicks per AD?"); //clicks per AD 
+var totalAds = prompt("What is the total number of Ads?"); //How many ads you have
 var adPay = [.20, .50];
 var adPayView = [200, 400];
 
