e58eb4af3b81e9c83af016bba4db5b7a62b8da1e
jgoldd <jgoldd@fullsail.edu>
2013-11-13 17:39:43 -0700

    other total expenses variable added

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -9,4 +9,4 @@
 //Can I survive this month on my SSI disability
 var totalIncome = prompt("What is your monthly income"); //Check from SSI check
 var rentTotal = prompt("What is your rent total?"); //Rent total amount
-
+var otherTotal = prompt("Any other necessary expenses?"); //Any other expenses
