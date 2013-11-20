b6a390d24a9515cea2a6baac181da91a9a44c793
jgoldd <jgoldd@fullsail.edu>
2013-11-19 21:23:35 -0700

    completed comments and equation

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -10,10 +10,11 @@
 var pi = 3.14159265359; //Pi variable
 var radius = 4; //radius 
 var circum = circleCir(pi,radius); //setting function to var for result
-console.log("The circumference of the circle is " + circum);
+console.log("The Circumference of the Circle is " + circum + "."); //console print out for circumference
 
-function circleCir(p,r){
+
+function circleCir(p,r){ //set-up for function and set parameters 
 	
-	return 2*p*r;
+	return 2*p*r; //equation return for circle circumference 
 	
 }
