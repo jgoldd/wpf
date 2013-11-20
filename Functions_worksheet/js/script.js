ccfc3a6963a042dc189586fd7add129b5267f788
jgoldd <jgoldd@fullsail.edu>
2013-11-19 21:37:45 -0700

    added parameters and arguments

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -12,7 +12,7 @@ console.log("----------- Circumference -----------");
 
 var pi = 3.14159265359; //Pi variable
 var radius = 4; //radius 
-var circum = circleCir(pi,radius); //setting function to var for result
+var circum = circleCir(pi,radius); //result variable
 console.log("The Circumference of the Circle is " + circum + "."); //console print out for circumference
 
 function circleCir(p,r){ //set-up for function and set parameters 
@@ -27,10 +27,10 @@ console.log("----------- Stung! -----------");
 
 var weight = 10; //weight variable
 var stingsPer = 8.666666667;
-var death = beeStings();
+var death = beeStings(weight, stingsPer); //result variable
 
-console.log("It takes " + + " bee stings to kill an animal."); //console print out
+console.log("It takes " + death + " bee stings to kill an animal."); //console print out
 
-function beeStings(){
-	return stingsPer * weight; //equation return for Bee Stings Per Pound
+function beeStings(w,s){
+	return s * w; //equation return for Bee Stings Per Pound
 }
