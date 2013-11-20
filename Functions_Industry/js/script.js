2e520026e2a0cd296b760f0a5745f4ff50f89350
jgoldd <jgoldd@fullsail.edu>
2013-11-20 16:16:19 -0700

    changed var to budget

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,4 +9,4 @@
 
 //Is a client worth it based on a $1000 a week minimum 
 
-var payPerHour = prompt("What is the total budget?"); //total price of project
+var budget = prompt("What is the total budget?"); //total price of project
