beb769c634a777144d1be4d29f8ed9932e91b27d
jgoldd <jgoldd@fullsail.edu>
2013-11-20 16:40:38 -0700

    set-up wacky functions structure

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,24 +1,11 @@
 /*
-	Assignment: Expressions Wacky Assignment
+	Assignment: Functions Wacky Assignment
 	
 	Author: Justin Goldman
 	
-	Date: November 7, 2013
+	Date: November 20, 2013
 	
 */
 
 
-//Aspect ratio of original image to new image size using pixels and new mage width
 
-var originalHeight = prompt("Enter start image height (px):"); // starting image demensions height
-var originalWidth = prompt("Enter start image width: (px)"); //starting image demensions width
-var newWidth = prompt("Enter target deminsion width: (px)"); //new deminsion width
-var newImage = "Your new image is "; // Concatenating for print out
-var pixels = " pixels."; //Concatenating for print out 
-
-
-//divide original height by original width to get new height
-var newHeight = (originalHeight / originalWidth) * newWidth;
-
-//print out to console new height 
-console.log(newImage + newHeight + pixels);
