26dd03c8f0ccaa053f346318f5d1d0f4163d94be
jgoldd <jgoldd@fullsail.edu>
2013-11-19 21:20:54 -0700

    adding comments

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -9,9 +9,11 @@
 //circumference of a circle = 2 * Pi * R
 var pi = 3.14159265359; //Pi variable
 var radius = 4; //radius 
+var circum = circleCir(pi,radius); //setting function to var for result
+console.log("The circumference of the circle is " + circum);
 
 function circleCir(p,r){
 	
-
+	return 2*p*r;
 	
 }
