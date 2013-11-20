1ee8b08102ca576af5676227dbc18d2a0012498d
jgoldd <jgoldd@fullsail.edu>
2013-11-19 21:27:59 -0700

    added var weight and set up 2d equation

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,15 +6,26 @@
 	Date: November 19, 2013
 	
 */
+console.log("----------- Circumference -----------");
+
 //circumference of a circle = 2 * Pi * R
+
 var pi = 3.14159265359; //Pi variable
 var radius = 4; //radius 
 var circum = circleCir(pi,radius); //setting function to var for result
 console.log("The Circumference of the Circle is " + circum + "."); //console print out for circumference
-
 
 function circleCir(p,r){ //set-up for function and set parameters 
 	
 	return 2*p*r; //equation return for circle circumference 
 	
 }
+
+
+
+console.log("----------- Stung! -----------");
+
+//How many Bee stings to kill an animal
+
+var weight = 10;
+
