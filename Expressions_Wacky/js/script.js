820bb25e1cafc5128f80905cb95724a0b92c8cdd
jgoldd <jgoldd@fullsail.edu>
2013-11-20 17:43:11 -0700

    set up first half of ternary

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -12,3 +12,5 @@ var priceMonthly = prompt("Price monthly?");
 var priceYearly = prompt("Price for yearly?");
 
 var priceAverage = priceMonthly * 12;
+
+(priceAverage === priceYearly) ? console.log("You will be paying the same amount for yearly as monthly.") :
