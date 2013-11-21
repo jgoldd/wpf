741f6b5a7c43582b7ddeed3e36754e853b78a52b
jgoldd <jgoldd@fullsail.edu>
2013-11-20 17:56:20 -0700

    added comment for equation

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -11,6 +11,6 @@
 var priceMonthly = prompt("Price monthly?"); //price monthly variable
 var priceYearly = prompt("Price for yearly?"); //price yearly variable
 
-var priceAverage = priceMonthly * 12;
+var priceAverage = priceMonthly * 12; //price monthly times number of months
 
 (priceAverage === priceYearly) ? console.log("You will be paying the same amount for yearly as monthly.") : (priceAverage > priceYearly) ? console.log("You will be paying more monthly. Go with the yearly!") : console.log("You will be paying more yearly than monthly. Go with monthly!");
